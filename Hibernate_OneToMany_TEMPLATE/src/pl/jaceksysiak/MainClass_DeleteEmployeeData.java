97282deb39926d48cba23074763723476c9f8fa0
Jacek <Jacek@192.168.1.100>
2020-03-28 09:52:53 +0100

    c

--- Hibernate_OneToMany_TEMPLATE/src/pl/jaceksysiak/MainClass_DeleteEmployeeData.java
+++ Hibernate_OneToMany_TEMPLATE/src/pl/jaceksysiak/MainClass_DeleteEmployeeData.java
@@ -1,17 +1,13 @@
 package pl.jaceksysiak;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import pl.jaceksysiak.entity.Address;
 import pl.jaceksysiak.entity.Employee;
 
-public class MainClass {
+public class MainClass_DeleteEmployeeData {
 	
 	public static void main(String[] args) {
 
@@ -26,29 +22,16 @@ public class MainClass {
 		Session session = factory.getCurrentSession();
 		
 		try {	
-		//-----------Employee	
-			Employee tmpEmployee1=new Employee();
-		//	tmpEmployee.setId(1);
-			tmpEmployee1.setName("Jack");
-
-			
-		//----------- Address
-			Address address1 = new Address();
-			address1.setCity("Warzawa");
-			address1.setCountry("Poland");
-			
-			Address address2 = new Address();
-			address1.setCity("Washington");
-			address1.setCountry("USA");
-			
 			// start a transaction
 			session.beginTransaction();
 			
-			// save the courses
-			session.save(tmpEmployee1);
-			session.save(address1);
-			session.save(address2);
+			//get Employee from DB
+			int tmpId = 1;
+			Employee tmpEmployee = session.get(Employee.class, tmpId);
 			
+			System.out.println("This employee to delete: " + tmpEmployee);
+			session.delete(tmpEmployee);
+		 
 			// commit transaction
 			session.getTransaction().commit();
 		}
